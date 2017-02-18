b2d3affe8067237df013695f2f5dcea74c631ce1
Justin Plock <jplock@smoketurner.com>
2017-02-18 10:46:46 -0500

    Clean up docker process

--- run.sh
+++ run.sh
@@ -2,4 +2,10 @@
 
 VERSION=`xmllint --xpath "//*[local-name()='project']/*[local-name()='version']/text()" pom.xml`
 
-docker run --rm=true -p 2003:2003 -p 8080:8080 smoketurner/graphiak:${VERSION}
+docker run \
+--name graphiak \
+--rm \
+-e PORT=2003 \
+-p 2003:2003 \
+-p 8080:8080 \
+smoketurner/graphiak:${VERSION}
